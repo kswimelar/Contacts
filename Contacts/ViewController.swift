7e82c688311cb5f5866a5e3514f71a5a50b106a4
kswimelar <35944507+kswimelar@users.noreply.github.com>
2018-10-05 14:36:25 -0500

    Add files via upload

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Kari Swimelar on 10/5/18.
+//  Copyright © 2018 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //Outlets
+    
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var txtFirst: UITextField!
+    @IBOutlet weak var txtLast: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtView: UITextView!
+    @IBOutlet weak var btnBack: UIButton!
+    
+    //Actions
+    
+    @IBAction func btnView(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnSave(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func btnBack(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
